04c00385d4514753149f11e93693c06b1620aa88
camilogs <camilo.gonzalez.col@gmail.com>
2023-11-03 15:03:17 -0500

    Add validations

--- Alters/Category.sql
+++ Alters/Category.sql
@@ -1,5 +1,7 @@
-use CemDatabase;
-CREATE TABLE Category (
-    [Id] int PRIMARY KEY IDENTITY,
-    [Name]  varchar(100) NOT NULL
-);
+IF NOT EXISTS(SELECT * FROM SYSOBJECTS WHERE TYPE = 'U' AND NAME = 'Transaction')
+BEGIN
+    CREATE TABLE [dbo].[Category] (
+        [Id] int PRIMARY KEY IDENTITY,
+        [Name]  varchar(100) NOT NULL
+    );
+END
--- Alters/Transaction.sql
+++ Alters/Transaction.sql
@@ -1,9 +1,11 @@
-use CemDatabase
+IF NOT EXISTS(SELECT * FROM SYSOBJECTS WHERE TYPE = 'U' AND NAME = 'Transaction')
+BEGIN
+    CREATE TABLE [dbo].[Transaction] (
+        [Id] int PRIMARY KEY IDENTITY,
+        [Description] varchar(200) NOT NULL,
+        [Amount] varchar(200) NOT NULL,
+        [TransactionType] int NOT NULL,   
+        [CategoryId] int FOREIGN KEY REFERENCES dbo.Category(Id) 
+    )
+END
 
-CREATE TABLE [Transaction] (
-    [Id] int PRIMARY KEY IDENTITY,
-    [Description] varchar(200) NOT NULL,
-    [Amount] varchar(200) NOT NULL,
-    [TransactionType] int NOT NULL,   
-    [CategoryId] int FOREIGN KEY REFERENCES dbo.Category(Id) 
-)
