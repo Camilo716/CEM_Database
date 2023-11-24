59f49d252974ed75aa2d4541d4acd366bb6d6a03
camilogs <camilo.gonzalez.col@gmail.com>
2023-11-24 15:49:18 -0500

    Make dynamic dataset

--- Seed/dataset.sql
+++ Seed/dataset.sql
@@ -1,45 +1,68 @@
--- INSERT para la tabla [Category]
-SELECT * FROM [Transaction]
-SELECT * FROM [Category]
-INSERT INTO [Category] (Name) VALUES ('Comida');
-INSERT INTO [Category] (Name) VALUES ('Ropa');
-INSERT INTO [Category] (Name) VALUES ('Tecnología');
-INSERT INTO [Category] (Name) VALUES ('Deportes');
-INSERT INTO [Category] (Name) VALUES ('Electrodomésticos');
-INSERT INTO [Category] (Name) VALUES ('Libros');
-INSERT INTO [Category] (Name) VALUES ('Salud');
-INSERT INTO [Category] (Name) VALUES ('Joyería');
-INSERT INTO [Category] (Name) VALUES ('Viajes');
-INSERT INTO [Category] (Name) VALUES ('Automóviles');
+-- Variables to store Category IDs
+DECLARE @CategoryId1 INT, @CategoryId2 INT, @CategoryId3 INT, @CategoryId4 INT, @CategoryId5 INT,
+        @CategoryId6 INT, @CategoryId7 INT, @CategoryId8 INT, @CategoryId9 INT, @CategoryId10 INT;
 
--- INSERT para la tabla [Transaction]
--- 30 transacciones ficticias
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Compra de alimentos', 50.25, 0, 1);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Camiseta nueva', 20.99, 0, 2);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Smartphone', 499.99, 0, 3);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Zapatillas deportivas', 75.50, 0, 4);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Lavadora', 399.00, 0, 5);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Libro de ciencia ficción', 12.99, 0, 6);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Consulta médica', 80.00, 0, 7);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Anillo de diamantes', 1500.00, 0, 8);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Billetes de avión', 600.00, 0, 9);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Pago del seguro del automóvil', 300.00, 0, 10);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Ingreso de salario', 2500.00, 1, 11);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Venta de antigüedades', 800.00, 1, 12);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Ingreso de alquiler', 1200.00, 1, 13);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Ingreso de inversión', 400.00, 1, 14);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Ingreso de bonificación', 300.00, 1, 15);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Gasto de entretenimiento', 50.00, 0, 16);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Compra de muebles', 400.00, 0, 17);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Ingreso de reembolso', 75.00, 1, 18);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Gasto de transporte', 30.00, 0, 19);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Ingreso de regalos', 100.00, 1, 20);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Gasto de educación', 200.00, 0, 21);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Ingreso de devolución de impuestos', 500.00, 1, 22);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Gasto de salud', 75.00, 0, 23);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Ingreso de venta de arte', 700.00, 1, 24);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Gasto de seguro médico', 120.00, 0, 25);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Ingreso de dividendos', 150.00, 1, 26);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Gasto de reparación de coche', 60.00, 0, 27);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Gasto de impuestos', 300.00, 0, 28);
-INSERT INTO [Transaction] (Description, Amount, TransactionType, CategoryId) VALUES ('Ingreso de venta de electrónicos', 450.00, 1, 29);
+-- Insert categories and save their IDs
+INSERT INTO [Category] ([Name]) VALUES ('Comida');
+SET @CategoryId1 = SCOPE_IDENTITY();
+
+INSERT INTO [Category] ([Name]) VALUES ('Ropa');
+SET @CategoryId2 = SCOPE_IDENTITY();
+
+INSERT INTO [Category] ([Name]) VALUES ('Tecnología');
+SET @CategoryId3 = SCOPE_IDENTITY();
+
+INSERT INTO [Category] ([Name]) VALUES ('Deportes');
+SET @CategoryId4 = SCOPE_IDENTITY();
+
+INSERT INTO [Category] ([Name]) VALUES ('Electrodomésticos');
+SET @CategoryId5 = SCOPE_IDENTITY();
+
+INSERT INTO [Category] ([Name]) VALUES ('Libros');
+SET @CategoryId6 = SCOPE_IDENTITY();
+
+INSERT INTO [Category] ([Name]) VALUES ('Salud');
+SET @CategoryId7 = SCOPE_IDENTITY();
+
+INSERT INTO [Category] ([Name]) VALUES ('Joyería');
+SET @CategoryId8 = SCOPE_IDENTITY();
+
+INSERT INTO [Category] ([Name]) VALUES ('Viajes');
+SET @CategoryId9 = SCOPE_IDENTITY();
+
+INSERT INTO [Category] ([Name]) VALUES ('Automóviles');
+SET @CategoryId10 = SCOPE_IDENTITY();
+
+-- Insert transactions related to the categories
+INSERT INTO [Transaction] (Description, Amount, TransactionType, Date, CategoryId) 
+VALUES ('Compra de alimentos', 50.25, 0, '2023-11-01', @CategoryId1);
+
+INSERT INTO [Transaction] (Description, Amount, TransactionType, Date, CategoryId) 
+VALUES ('Camiseta nueva', 20.99, 0, '2023-11-02', @CategoryId2);
+
+INSERT INTO [Transaction] (Description, Amount, TransactionType, Date, CategoryId) 
+VALUES ('Smartphone', 499.99, 0, GETDATE(), @CategoryId3);
+
+INSERT INTO [Transaction] (Description, Amount, TransactionType, Date, CategoryId) 
+VALUES ('Zapatillas deportivas', 75.50, 0, GETDATE(), @CategoryId4);
+
+INSERT INTO [Transaction] (Description, Amount, TransactionType, Date, CategoryId) 
+VALUES ('Lavadora', 399.00, 0, GETDATE(), @CategoryId5);
+
+INSERT INTO [Transaction] (Description, Amount, TransactionType, Date, CategoryId) 
+VALUES ('Libro de ciencia ficción', 12.99, 0, GETDATE(), @CategoryId6);
+
+INSERT INTO [Transaction] (Description, Amount, TransactionType, Date, CategoryId) 
+VALUES ('Consulta médica', 80.00, 0, GETDATE(), @CategoryId7);
+
+INSERT INTO [Transaction] (Description, Amount, TransactionType, Date, CategoryId) 
+VALUES ('Ingreso de salario', 2500.00, 1, GETDATE(), @CategoryId8);
+
+INSERT INTO [Transaction] (Description, Amount, TransactionType, Date, CategoryId) 
+VALUES ('Venta de antigüedades', 800.00, 1, GETDATE(), @CategoryId9);
+
+INSERT INTO [Transaction] (Description, Amount, TransactionType, Date, CategoryId) 
+VALUES ('Ingreso de alquiler', 1200.00, 1, GETDATE(), @CategoryId10);
+
+INSERT INTO [Transaction] (Description, Amount, TransactionType, Date, CategoryId) 
+VALUES ('Ingreso de inversión', 400.00, 1, GETDATE(), @CategoryId1);
