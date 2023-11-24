adac69db30d9a92f831afd21db22e5f76a0c611e
camilogs <camilo.gonzalez.col@gmail.com>
2023-11-24 15:53:49 -0500

    execute dataset into reinitializer script

--- reinitializeSchema.sh
+++ reinitializeSchema.sh
@@ -30,3 +30,10 @@ do
   echo "Ejecutando $archivo_sql..." >> "$archivo_log"
   sqlcmd -S "$server" -U "$user" -P "$password" -d "$database" -i "$archivo_sql" >> "$archivo_log" 2>&1
 done
+
+find Seed -type f -name '*.sql' -print0 | while IFS= read -r -d $'\0' archivo_sql
+do
+  echo "Ejecutando $archivo_sql..."
+  echo "Ejecutando $archivo_sql..." >> "$archivo_log"
+  sqlcmd -S "$server" -U "$user" -P "$password" -d "$database" -i "$archivo_sql" >> "$archivo_log" 2>&1
+done
