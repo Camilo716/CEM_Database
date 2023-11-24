0d8d33ae8463205d3a4920b49916698636ecc65c
camilogs <camilo.gonzalez.col@gmail.com>
2023-11-24 09:29:52 -0500

    Fix BalanceReport

--- Procedures/Reports/BalanceReport.sql
+++ Procedures/Reports/BalanceReport.sql
@@ -1,14 +1,21 @@
 CREATE OR ALTER PROCEDURE [dbo].[MonthlyBalanceReport]
+    @TotalEarned DECIMAL(18, 2) OUTPUT,
+    @TotalSpent DECIMAL(18, 2) OUTPUT
 AS
 BEGIN TRANSACTION
     SELECT 
         [Category].[Name] AS Category,
-        SUM(CASE WHEN [Transaction].[TransactionType] = 0 THEN [Transaction].[Amount] ELSE 0 END) AS Earned,
-        SUM(CASE WHEN [Transaction].[TransactionType] = 1 THEN [Transaction].[Amount] ELSE 0 END) AS Spent
+        SUM(CASE WHEN [Transaction].[TransactionType] = 1 THEN [Transaction].[Amount] ELSE 0 END) AS Earned,
+        SUM(CASE WHEN [Transaction].[TransactionType] = 0 THEN [Transaction].[Amount] ELSE 0 END) AS Spent
     FROM [Transaction]
         RIGHT JOIN [Category]
             ON [Category].[Id] = [Transaction].[CategoryId]
     GROUP BY 
         [Transaction].[CategoryId],
         [Category].[Name]
+
+    SELECT 
+        @TotalEarned = SUM(CASE WHEN [Transaction].[TransactionType] = 1 THEN [Transaction].[Amount] ELSE 0 END),
+        @TotalSpent = SUM(CASE WHEN [Transaction].[TransactionType] = 0 THEN [Transaction].[Amount] ELSE 0 END)
+    FROM [Transaction]    
 COMMIT
